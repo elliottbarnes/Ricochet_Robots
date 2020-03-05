e3e6fc6415c962d20c19d58a8582a6f9a3d21e25
Jorge Rivas <jorgerivas.adoc@gmail.com>
2020-03-05 12:39:49 -0330

    recreated project

---
+++ Ricochet Robots/src/controller/Bid.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class Bid {
+	int bid1;
+
+	public int getBid1() {
+		return bid1;
+	}
+
+	public void setBid1(int bid1) {
+		this.bid1 = bid1;
+	}
+
+}
---
+++ Ricochet Robots/src/view/HintButton.java
@@ -0,0 +1,15 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+
+public class HintButton extends JButton {
+
+	public HintButton() {
+		setText("Hint!");
+        setLocation(0, 0);
+        setSize(100, 30);
+        setEnabled(true);
+		
+	}
+
+}
