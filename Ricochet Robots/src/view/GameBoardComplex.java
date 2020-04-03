d030c71e66d214c8f66f2040d731ac2adbdcf1c8
Jorge Rivas <jorgerivas.adoc@gmail.com>
2020-04-02 23:46:40 -0230

    completed complex board

--- Ricochet Robots/src/view/GameBoardComplex.java
+++ Ricochet Robots/src/view/GameBoardComplex.java
@@ -81,23 +81,23 @@ public class GameBoardComplex implements Runnable {
 		handler.addObject(new ComplexBarrier(37, 480, 37, 37, ID.ComplexBarrier8, 2));
 		
 		//Tokens
-//		handler.addObject(new Token(334, 74, 28, 28, ID.Token, 1));
-//		handler.addObject(new Token(37, 80, 28, 28, ID.Token, 2));
-//		handler.addObject(new Token(298, 377, 28, 28, ID.Token, 3));
-//		handler.addObject(new Token(118, 526, 28, 28, ID.Token, 4)); 
-//		handler.addObject(new Token(39, 335, 28, 28, ID.Token, 5));
-//		handler.addObject(new Token(488, 410, 28, 28, ID.Token, 6)); 
-//		handler.addObject(new Token(155, 43, 28, 28, ID.Token, 7));
-//		handler.addObject(new Token(409, 230, 28, 28, ID.Token, 8));
-//		handler.addObject(new Token(118, 226, 28, 28, ID.Token, 9));
-//		handler.addObject(new Token(155, 372, 28, 28, ID.Token, 10));
-//		handler.addObject(new Token(487, 45, 28, 28, ID.Token, 11));
-//		handler.addObject(new Token(522, 335, 28, 28, ID.Token, 12));
-//		handler.addObject(new Token(377, 487, 28, 28, ID.Token, 13));
-//		handler.addObject(new Token(526, 188, 28, 28, ID.Token, 14));
-//		handler.addObject(new Token(188, 488, 28, 28, ID.Token, 15));
-//		handler.addObject(new Token(225, 113, 28, 28, ID.Token, 16));
-//		handler.addObject(new Token(186, 300, 28, 28, ID.Token, 17));
+		handler.addObject(new Token(297, 150, 28, 28, ID.Token1, 1));
+		handler.addObject(new Token(75, 225, 28, 28, ID.Token2, 2));
+		handler.addObject(new Token(446, 412, 28, 28, ID.Token3, 3));
+		handler.addObject(new Token(77, 334, 28, 28, ID.Token4, 4));
+		handler.addObject(new Token(115, 485, 28, 28, ID.Token5, 5));
+		handler.addObject(new Token(486, 410, 28, 28, ID.Token6, 6)); 
+		handler.addObject(new Token(40, 114, 28, 28, ID.Token7, 7));
+		handler.addObject(new Token(483, 226, 28, 28, ID.Token8, 8));
+		handler.addObject(new Token(115, 225, 28, 28, ID.Token9, 9));
+		handler.addObject(new Token(113, 448, 28, 28, ID.Token10, 10));
+		handler.addObject(new Token(375, 42, 28, 28, ID.Token11, 11));
+		handler.addObject(new Token(373, 336, 28, 28, ID.Token12, 12));
+		handler.addObject(new Token(337, 487, 28, 28, ID.Token13, 13));
+		handler.addObject(new Token(484, 188, 28, 28, ID.Token14, 14));
+		handler.addObject(new Token(188, 521, 28, 28, ID.Token15, 15));
+		handler.addObject(new Token(225, 150, 28, 28, ID.Token16, 16));
+		handler.addObject(new Token(260, 375, 28, 28, ID.Token17, 17));
 		
 		//Robots
 		handler.addObject(new Robot(10, 10, 22, 22, ID.Robot, handler));
